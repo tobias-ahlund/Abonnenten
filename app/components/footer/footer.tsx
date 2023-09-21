fdc43cb171baf0a5351f683579ded1cec77e0fda
tobias-ahlund <tobias.ahlund@gmail.com>
2023-09-21 15:09:06 +0200

    further development of footer

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -1,10 +1,16 @@
+"use client"
+
 import styles from './styles.module.css'
 import Link from "next/link"
 import Image from "next/image"
 import { footerLinkArray } from "./footerLink"
+import { usePathname } from "next/navigation"
 
+import DashboardLogoActive from "app/public/images/logo_bottom-nav-active.svg"
 
 export default function Footer(){
+    const path = usePathname();
+   
     return (
         <footer className={styles.footer}>
             <nav>
@@ -12,7 +18,10 @@ export default function Footer(){
                     {footerLinkArray.map(footerLink => {return(
                     <li>
                         <Link href={footerLink.href}>
-                            <Image priority src={footerLink.src} alt={footerLink.alt}/>
+                            <Image priority 
+                                src={path === "/dashboard" && footerLink.href === "/dashboard" ? DashboardLogoActive : footerLink.src}
+                                alt={footerLink.alt} 
+                            />
                         </Link>
                     </li>)})}
                 </ul>
--- app/components/footer/footerLink.tsx
+++ app/components/footer/footerLink.tsx
@@ -1,9 +1,16 @@
+"use client"
+
 import Image from "next/image";
 import Link from "next/link";
 import DashboardLogo from "app/public/images/logo_bottom-nav.svg"
 import SubscriptionsLogo from "app/public/images/logo_bottom-nav-1.svg"
 import SupportLogo from "app/public/images/logo_bottom-nav-2.svg"
 import ProfileLogo from "app/public/images/logo_bottom-nav-3.svg"
+
+import DashboardLogoActive from "app/public/images/logo_bottom-nav-active.svg"
+import SubscriptionsLogoActive from "app/public/images/logo_bottom-nav-1-active.svg"
+import SupportLogoActive from "app/public/images/logo_bottom-nav-2-active.svg"
+import ProfileLogoActive from "app/public/images/logo_bottom-nav-3-active.svg"
 
 type FooterLink = {
     href: string;
