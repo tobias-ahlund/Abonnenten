1be74fdebae2d4605ff60baed516ae10a8e55947
alun0511 <78014508+alun0511@users.noreply.github.com>
2023-09-28 14:01:25 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,3 +2,11 @@
 const nextConfig = {}
 
 module.exports = nextConfig
+
+const withPWA = require("@ducanh2912/next-pwa").default({
+    dest: "public",
+  });
+  
+  module.exports = withPWA({
+    // Your Next.js config
+  });
