578a08ffecdf5cd945f59f697e0aa07daecfca57
tobias-ahlund <tobias.ahlund@gmail.com>
2023-09-29 00:21:32 +0200

    added menu

--- app/components/Subscriptions/subscriptions.tsx
+++ app/components/Subscriptions/subscriptions.tsx
@@ -9,12 +9,11 @@ import Placeholder from "app/public/images/Placeholder.svg";
 import Netflix from "/app/public/images/netflix.png";
 import Apple_tv from "/app/public/images/apple_tv.png";
 import Spotify from "/app/public/images/spotify.svg";
-
-interface Subscription {
-    subscription_presets: {
-      logo: string;
-    };
-  }
+import Tele2 from "@/app/public/images/tele2.svg";
+import Friskis from "@/app/public/images/friskis.svg";
+import GoteborgEnergi from "@/app/public/images/goteborg_energi.svg";
+import LuleaEnergi from "@/app/public/images/lulea_energi.svg";
+import Arrow from "@/app/public/images/arrow.svg";
 
 export default async function Subscriptions() {
 const supabase = createServerComponentClient({ cookies })
@@ -41,6 +40,49 @@ let { data: subscriptions, error } = await supabase
                     <p>{totalCostYear} kr</p>
                 </div>
             </section>
+
+            <div className={styles.dropdown}>
+                <div className={styles.dropdownButton}>
+                    <p>Se abonnemang</p>
+                </div>
+                <div>
+                    <Image className={styles.arrow} src={Arrow} alt="dropdown arrow button" />
+                </div>
+            </div>
+            <ul className={styles.subsDropdown}>
+                {subscriptions?.map((subscription, index) => (
+                    <li key={index}>
+                        <div className={styles.subDropdown}>
+                            <div className={styles.logoDropdownWrapper}>
+                                <Image
+                                    key={index}
+                                    src={
+                                        subscription.name.includes("Netflix") 
+                                        ? Netflix 
+                                        : subscription.name.includes("Apple")
+                                        ? Apple_tv 
+                                        : subscription.name.includes("Spotify") 
+                                        ? Spotify 
+                                        : subscription.name.includes("Göteborg")
+                                        ? GoteborgEnergi
+                                        : subscription.name.includes("Luleå")
+                                        ? LuleaEnergi
+                                        : subscription.name.includes("Friskis")
+                                        ? Friskis
+                                        : subscription.name.includes("Tele2")
+                                        ? Tele2
+                                        : Placeholder
+                                    }
+                                    alt="subscription icon"
+                                />
+                            </div>
+                            <p>{subscription.name}</p>
+                        </div>
+                        <hr className={styles.hr} />
+                    </li>
+                ))}
+            </ul>
+
             <ul className={styles.subsWrapper}>
                 {subscriptions?.map((subscription, index) => (
                     <li key={index}>
@@ -55,6 +97,14 @@ let { data: subscriptions, error } = await supabase
                                         ? Apple_tv 
                                         : subscription.name.includes("Spotify") 
                                         ? Spotify 
+                                        : subscription.name.includes("Göteborg")
+                                        ? GoteborgEnergi
+                                        : subscription.name.includes("Luleå")
+                                        ? LuleaEnergi
+                                        : subscription.name.includes("Friskis")
+                                        ? Friskis
+                                        : subscription.name.includes("Tele2")
+                                        ? Tele2
                                         : Placeholder
                                     }
                                     alt="subscription icon"
