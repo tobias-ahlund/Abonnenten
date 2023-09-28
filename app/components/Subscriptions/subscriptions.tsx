d68235cb9c88c722891ec864d5cb26b944bc318c
tobias-ahlund <tobias.ahlund@gmail.com>
2023-09-28 12:59:34 +0200

    logos added to subs

--- app/components/Subscriptions/subscriptions.tsx
+++ app/components/Subscriptions/subscriptions.tsx
@@ -6,18 +6,23 @@ import DeleteSubscription from "../deleteSubscription";
 import styles from "./styles.module.css";
 import Image from "next/image";
 import Placeholder from "app/public/images/Placeholder.svg";
+import Netflix from "/app/public/images/netflix.png";
+import Apple_tv from "/app/public/images/apple_tv.png";
+import Spotify from "/app/public/images/spotify.svg";
 
 export default async function Subscriptions() {
 const supabase = createServerComponentClient({ cookies })
 
 let { data: subscriptions, error } = await supabase
     .from('subscriptions')
-    .select("id,name,cost")
+    .select("id, name, cost, subscription_presets ( logo )")
+
+    console.log(subscriptions && subscriptions)
 
     const items = subscriptions;
     const totalCostMonth = items?.reduce((total, item) => total + item.cost, 0);
     const totalCostYear = (totalCostMonth && totalCostMonth * 12);
-    
+
     return (
         <>
             <hr />
@@ -35,14 +40,21 @@ let { data: subscriptions, error } = await supabase
             <ul className={styles.subsWrapper}>
                 {subscriptions?.map((subscription, index) => (
                     <li key={index}>
-                        <div>
-                            {/* <span>Id: {subscription.id}</span>
-                            <br /> */}
-                            <Image src={Placeholder} alt="subscription icon" />
+                        <div className={styles.subWrapper}>
+                            <div className={styles.logoWrapper}>
+                                <Image
+                                    key={index}
+                                    src={subscription.subscription_presets.logo === "Netflix" ? Netflix 
+                                    : subscription.subscription_presets.logo === "Apple_tv" ? Apple_tv 
+                                    : subscription.subscription_presets.logo === "Spotify" ? Spotify 
+                                    : Placeholder}
+                                    alt="subscription icon"
+                                />
+                            </div>
                             <p>{subscription.name}</p>
                             <p>{subscription.cost} kr/mån</p>
                         </div>
-                        <div>
+                        <div className={styles.wrapperStatus}>
                             <p>Ingen åtgärd<br /> nödvändig</p>
                         </div>
                     </li>
